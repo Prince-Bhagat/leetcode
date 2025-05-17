b7e22d86c62d4e5595c44ed3b569253f7f41ec68
Prince <bhagatprince66@gmail.com>
2025-05-17 10:02:12 +0530

    Time: 31 ms (5.29%), Space: 44.6 MB (98.31%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,64 +1,29 @@
 class Solution {
-    public class TopologicalSort {
-    int [][] edges;
-    int [] visited; // 0 not visited ; 1 visited(still processing); 2 (visited and process)
-    boolean isCycleExist;
-
-
-    public TopologicalSort(int[][] edges, int numberOfVertex) {
-        this.edges = edges;
-        this.numberOfVertex = numberOfVertex;
-        this.visited = new int[numberOfVertex];
-
-    }
-
-    public List<Integer> getOrder(){
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < numberOfVertex; i++) {
-            boolean nocycle = recursionDfsReverse(i,list);
-            if( !nocycle )break;
-        }
-        return list;
-
-    }
-
-    public boolean recursionDfsReverse(int source, List<Integer> result){
-
-        if(visited[source] == 1 ){
-            isCycleExist= true;
-            return false;
-        }
-        if(visited[source] == 2) return true;
-
-        visited[source] = 1;
-        List<Integer> listOfNeighbour = getNeighbours(source, edges);
-        if (!listOfNeighbour.isEmpty()){
-            for (int neighbour : listOfNeighbour){
-                recursionDfsReverse(neighbour, result);
-            }
-        }
-        result.add(source);
-        visited[source] = 2;
-        return true;
-
-    }
-
-    private List<Integer> getNeighbours(int currentNode, int[][] edges) {
-        List<Integer> list = new ArrayList<>();
-        for (int [] edge : edges){
-            if(edge[0] == currentNode){
-                list.add(edge[1]);
-            }
-        }
-        return list;
-    }
-    int numberOfVertex;
-
-}
-
+    int [] nodeArray;
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        TopologicalSort topologicalSort = new TopologicalSort(prerequisites,numCourses);
-        topologicalSort.getOrder();
-        return !topologicalSort.isCycleExist;
+        nodeArray = new int[numCourses]; // 0 : Not Visited, 1: Visted and in progresses, 2 Visited and completed
+        for (int i = 0; i < numCourses; i++) {
+            boolean isSuccessDFS = dfs(prerequisites,i);
+            if (!isSuccessDFS) return isSuccessDFS;
+
+        }
+        return true;
+    }
+
+    private boolean dfs(int[][] prerequisites, int i) {
+        if(nodeArray[i]== 2) return true;
+        if(nodeArray[i] == 1) return false;
+
+        nodeArray[i] = 1;
+        for (int [] edge : prerequisites){
+            int start = edge[1];
+            int end = edge[0];
+            if(start== i){
+                boolean isSuccessfull = dfs(prerequisites,end);
+                if(!isSuccessfull) return false;
+            }
+        }
+        nodeArray[i] = 2;
+        return true;
     }
 }
