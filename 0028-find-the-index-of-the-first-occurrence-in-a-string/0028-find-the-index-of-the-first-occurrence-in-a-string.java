fb6d38d2d5799abe9323f11ff5f35ee11d1d80ae
Prince <bhagatprince66@gmail.com>
2025-06-15 10:40:21 +0530

    Time: 1 ms (22.86%), Space: 41.5 MB (64.41%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -8,8 +8,17 @@ class Solution {
     }
     public int strStr(String text, String pattern) {
         int patternHash = computeHash(0, pattern.length() -1, pattern);
-        for (int i = 0; i < text.length()  &&  (text.length() >= pattern.length() + i); i++) {
-            int currentHash = computeHash(i, i+ pattern.length()-1, text);
+        int currentHash =0;
+        for (int i = 0; i < text.length() && (text.length() >= pattern.length() + i); i++) {
+            if(currentHash == 0){
+                for (int j = 0; j < pattern.length(); j++) {
+                    currentHash += text.charAt(j+i);
+                }
+            }else{
+                currentHash = currentHash - text.charAt(i-1) + text.charAt(i+ pattern.length()-1);
+
+            }
+
             if(currentHash == patternHash){
                 boolean isFound = false;
                 for (int j = 0; j < pattern.length(); j++) {
