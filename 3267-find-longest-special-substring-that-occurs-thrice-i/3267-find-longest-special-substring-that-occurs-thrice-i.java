a304b86154611d1d19b47387c500eb257474a18f
Prince <bhagatprince66@gmail.com>
2025-10-17 18:21:50 +0530

    Time: 6 ms (67.49%), Space: 45 MB (61.13%) - LeetHub

--- 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.java
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.java
@@ -1,31 +1,48 @@
 class Solution {
     public int maximumLength(String input) {
-        Map<String, Integer> map =  new HashMap<>();
-        for (int i = 0; i < input.length(); i++) {
-            Character ch = input.charAt(i);
-            int counter = i;
-            while(counter < input.length() && ch == input.charAt(counter)){
-                counter++;
-                String substring = input.substring(i, counter);
+        Map<String, Integer> map = new HashMap<>();
+        int start =0;
+        int end = 0;
+        while (end < input.length()){
+            Character startCharacter = input.charAt(start);
+            while(end < input.length() && input.charAt(end)== startCharacter){
+                end++;
+            }
+            String substring = input.substring(start, end);
+            if((end - start) > 1){
+                // Generate Substring
+                int size = end - start;
+                int counter = size;
+                for (int i = 1; i <= size; i++) {
+                    Character ch = input.charAt(start);
+                    String currentsubstring = String.valueOf(ch).repeat(i);
+                    if(map.containsKey(currentsubstring)){
+                        map.put(currentsubstring, map.get(currentsubstring)+counter);
+                    }else{
+                        map.put(currentsubstring, counter);
+                    }
+                    counter--;
+                }
+
+            }else{
                 if(map.containsKey(substring)){
-                    map.put(substring, map.get(substring) + 1);
+                    map.put(substring, map.get(substring)+1);
                 }else{
                     map.put(substring, 1);
                 }
-
             }
 
+            start = end;
         }
 
-        int max = 3;
-        String finalString ="";
-        for(Map.Entry<String, Integer> entry : map.entrySet()){
-            if(entry.getValue() >= max &&  entry.getKey().length() >  finalString.length()){
+        String finalString = "";
+        for (Map.Entry<String,Integer> entry : map.entrySet()){
+            if(entry.getValue() >= 3 && entry.getKey().length() > finalString.length() ){
                 finalString = entry.getKey();
             }
         }
         if(finalString.length() == 0){
-            return -1;
+            return  -1;
         }else{
             return finalString.length();
         }
