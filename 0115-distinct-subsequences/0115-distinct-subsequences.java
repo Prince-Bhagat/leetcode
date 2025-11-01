b83aba7dba36462319034677579d0868913d052c
Prince <bhagatprince66@gmail.com>
2025-11-01 22:34:19 +0530

    Time: 16 ms (87.89%), Space: 54.1 MB (12.63%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -5,16 +5,17 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             table[i][0] = 1;
         }
-        for (int i = 1; i <= s.length() ; i++) {
-            for (int j = 1; j <= t.length(); j++) {
-                if(s.charAt(i -1) == t.charAt(j -1)){
-                    table[i][j] = table[i-1][j-1] + table[i-1][j];
+        for (int i = 1; i <= t.length() ; i++) {
+            for (int j = i; j <= s.length(); j++) {
+                if(s.charAt(j -1) == t.charAt(i -1)){
+                    table[j][i] = table[j-1][i-1] + table[j-1][i];
                 }else {
-                    table[i][j] = table[i-1][j];
+                    table[j][i] = table[j-1][i];
                 }
 
             }
         }
-        return table[s.length()][t.length()];   
+        return table[s.length()][t.length()];
+   
     }
 }
